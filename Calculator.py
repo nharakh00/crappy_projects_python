1e99cf8f05b3e35e440ddba2b4162450a1718bda
nharakh00 <45838914+nharakh00@users.noreply.github.com>
2023-11-08 17:05:27 -0800

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,74 @@
+# Evaluating Expression with Eval
+
+while(1):
+    print("Enter an expression you would like to evaluate.(press q to quit)")
+    exp = input()
+
+    if exp == 'q':
+        break
+    try:
+        result = eval(exp)
+        print("result: " + str(result))
+    except:
+        print("Error: Invalid Expression")
+
+
+"""
 import sys
 
+def Calculate(first_num, operator, second_num):
+
+    try:
+        first_num = float(first_num)
+        second_num = float(second_num)
+    except ValueError:
+        return "Error: Invalid Number" 
+    
+    if operator == '+':
+        value = first_num + second_num
+    elif operator == '-':
+        value = first_num - second_num
+    elif operator == '*':
+        value = first_num * second_num
+    elif operator == '/':
+        if second_num == 0:
+            return "Error: Division By Zero"
+        else:
+            value = first_num / second_num
+    else:
+        return "Invalid Operator"
+
+    return value
+
+# The main function
+
+while(1):
+    print("Enter The first Number.(Enter q) to quit")
+    first_num = input()
+
+    if first_num == 'q':
+        break
+
+    print("Enter Operator. (+,-,*,/)")
+    operator = input()
+
+    print("Enter Second Number.")
+    second_num = input()
+
+    value = Calculate(first_num, operator, second_num)
+    print(value)
+
+"""
+
+"""
+import sys
+
+# Receiving input from user
 print("Enter the expression you would like to evaluate.(NO SPACES)")
 expression = input()
 
 
-# This could maybe be the first function finding the first value and then iterating
+# Finding result of first operation
 temp = 0
 ans = 0
 try:
@@ -20,39 +84,36 @@ try:
     elif op == '/':
         temp = f_num / s_num
     else:
-        print("Error. Invalid Expression")
         sys.exit()
 except:
     print("Error. Invalid Expression")
     sys.exit()
-
+    
+# Returning result if only one operation
 if len(expression) == 3:
     ans = temp
     print("This expression evaluates to: "  + str(ans))
+
+# Otherwise perforiming iteratvley
 else:
-    print("There is more work to do beyond this point.")
+    try:
+        for i in range(3,len(expression)):
+            if expression[i] == '+':
+                temp = temp + float(expression[i+1])
+            elif expression[i] == '-':
+                temp = temp - float(expression[i+1])
+            elif expression[i] == '*':
+                temp = temp * float(expression[i+1])
+            elif expression[i] == '/':
+                temp = temp / float(expression[i+1])
+    except:
+        print("Error. Invalid Expression")
+        sys.exit()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Returning Result 
+    ans = temp
+    print("This expression evaluates to: " + str(ans))
+"""
 
 
 """
